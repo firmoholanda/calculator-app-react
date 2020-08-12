0c3ca27d0b2094949a4c64b64c25530cd96ec370
firmo holanda <firmo@viize.com>
2020-08-12 18:12:02 -0300

    organize folder structure and add bootstrap to app

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import MyJumbotron from './MyJumbotron';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <MyJumbotron />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Footer extends React.Component {
+  render() {
+    return (
+      <footer className="footer">
+        <hr />
+        <small>
+          calculator-app-react created by:
+          <a href="https://github.com/firmoholanda">firmo holanda</a>
+        </small>
+      </footer>
+    );
+  }
+}
