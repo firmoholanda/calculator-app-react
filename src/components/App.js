d48d47be3289f7fe5a20bcab46b7025818a56807
firmo holanda <firmo@viize.com>
2020-08-13 10:42:47 -0300

    add Display and ButtonPanel components to App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
 import Jumbotron from './Jumbotron';
+import Display from './Display';
+import ButtonPanel from './ButtonPanel';
 import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
       <Jumbotron />
+      <Display />
+      <ButtonPanel />
       <Footer />
     </div>
   );
