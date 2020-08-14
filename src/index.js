3d3e6e9ac31690aed8b45ff73f78b288e66e38ad
firmo holanda <firmo@viize.com>
2020-08-14 12:58:26 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
 import App from './components/App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,7 +13,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
