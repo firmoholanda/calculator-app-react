3d9c09c0dbf21a2b912adbbcf50b26a3c785529d
firmo holanda <firmo@viize.com>
2020-08-17 12:21:50 -0300

    fix eslint erros

--- src/index.js
+++ src/index.js
@@ -6,10 +6,9 @@ import './index.css';
 
 import App from './components/App';
 
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
