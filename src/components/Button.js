0ac19bef0fd3d913e462c87563844388923af6ea
firmo holanda <firmo@viize.com>
2020-08-18 18:52:17 -0300

    fix eslint errors

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,12 +4,11 @@ import PropTypes from 'prop-types';
 const Button = ({
   name, color, wide,
 }) => {
-
   const style = {
     width: wide ? '50%' : '25%',
     backgroundColor: color,
   };
-  
+
   return (
     <button type="button" style={style}>
       {name}
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,7 +3,6 @@ import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 
-
 function Calculator() {
   return (
     <div className="calculator w-50 h-50 mt-4 mx-auto shadow bg-white rounded">
