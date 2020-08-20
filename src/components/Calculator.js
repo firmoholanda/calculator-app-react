16337d06aaeaa4c0c96b766d5605e8f2b574cec4
firmo holanda <firmo@viize.com>
2020-08-20 16:41:48 -0300

    add calculate to calculator component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,6 +3,8 @@ import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 
+import Calculate from '../logic/calculate';
+
 function Calculator() {
   return (
     <div className="calculator">
