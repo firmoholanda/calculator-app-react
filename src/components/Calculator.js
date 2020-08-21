c270f7eac291a13bbbae7d53d31f300d86e9ddf6
firmo holanda <firmo@viize.com>
2020-08-21 13:07:27 -0300

    add handleClick and pass the total or next value to Display

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,13 +1,25 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
+import Calculate from '../logic/calculate';
 
 function Calculator() {
+
+  const [state, setState] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const handleClick = (buttonName) => {
+    setState(Calculate(state, buttonName));
+  };
+
+
   return (
     <div className="calculator">
-      <Display />
-      <ButtonPanel />
+      <Display result={state.next || state.total || 0} />
+      <ButtonPanel clickHandler={handleClick} />
     </div>
   );
 }
